416739eaa77a9a6e584a1b9f5243598f0d33856b
Matias Berrutti <berrutti@gmail.com>
2025-02-16 18:49:10 +0100

    Try regular form

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,7 @@
-import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 
 function App() {
-  const [formData, setFormData] = useState({
-    NAME: "",
-    EMAIL: "",
-    PHONE: "",
-    DESCRIPTION: "",
-  });
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbxcBkQQpST_E_57NkBudcC4mPoeuFOyxCJFp8nUSgaQjMY4l-pXPwt0aG3qJAJFcH_O/exec",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }
-      );
-      if (response.ok) {
-        alert("Mensaje enviado!");
-        setFormData({
-          NAME: "",
-          EMAIL: "",
-          PHONE: "",
-          DESCRIPTION: "",
-        });
-      } else {
-        alert("No se pudo enviar el mensaje.");
-      }
-    } catch (error) {
-      console.error("Error para enviar el mensaje:", error);
-    }
-  };
-
   return (
     <div className="app-container">
       <Navbar />
@@ -100,36 +53,16 @@ function App() {
 
       <section id="contacto" className="section">
         <h1>Contacto</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            name="NAME"
-            type="text"
-            placeholder="Nombre"
-            value={formData.NAME}
-            onChange={handleChange}
-            required
-          />
-          <input
-            name="EMAIL"
-            type="email"
-            placeholder="Email"
-            value={formData.EMAIL}
-            onChange={handleChange}
-            required
-          />
-          <input
-            name="PHONE"
-            type="tel"
-            placeholder="Telefono"
-            value={formData.PHONE}
-            onChange={handleChange}
-            required
-          />
+        <form
+          action="https://script.google.com/macros/s/AKfycbwLDo7TII8YJq0Pi3XKMmdXnM4OpPRDTe2EFc83knHAekbNT-f1WL9Ricq03zswnmU1/exec"
+          method="POST"
+        >
+          <input name="NAME" type="text" placeholder="Nombre" required />
+          <input name="EMAIL" type="email" placeholder="Email" required />
+          <input name="PHONE" type="tel" placeholder="Telefono" required />
           <textarea
             name="DESCRIPTION"
             placeholder="Mensaje"
-            value={formData.DESCRIPTION}
-            onChange={handleChange}
             required
           ></textarea>
           <button type="submit">Enviar</button>
